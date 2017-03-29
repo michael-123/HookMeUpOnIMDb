ea9460e693807c46b529636492f96a14c1aeb933
michael-123 <dale.cooper@posteo.de>
2017-03-29 21:40:58 +0200

    Update settings_dummy.py.

--- settings_dummy.py
+++ settings_dummy.py
@@ -1,6 +1,6 @@
-CSV_DIR = '/home/michael-123/PycharmProjects/HookMeUpOnIMDb-Data/tables'
-MOVIE_HTML_DIR = '/home/michael-123/PycharmProjects/HookMeUpOnIMDb-Data/movies'
-SOUNDTRACK_HTML_DIR = '/home/michael-123/PycharmProjects/HookMeUpOnIMDb-Data/soundtracks'
+CSV_DIR = '/path/to/output/directory/for/csv/files'
+MOVIE_HTML_DIR = '/path/to/movie/html/database/folder'
+SOUNDTRACK_HTML_DIR = '/path/to/soundtrack/html/database/folder'
 
 # CSV file names
 CSV_MOVIES = '{}/{}'.format(CSV_DIR, 'movies.csv')
@@ -9,9 +9,9 @@ CSV_RECOMMENDATIONS = '{}/{}'.format(CSV_DIR, 'recommendations.csv')
 CSV_WATCHLIST = '{}/{}'.format(CSV_DIR, 'watchlist.csv')
 
 # IMDB info
-IMDB_USER_ID = 'kalr_123'
-RATINGS_FILE_PATH = '/home/michael-123/PycharmProjects/HookMeUpOnIMDb-Data/ratings.csv'
-WATCHLIST_FILE_PATH = '/home/michael-123/PycharmProjects/HookMeUpOnIMDb-Data/watchlist.csv'
+IMDB_USER_ID = 'user_name_on_IMDb'
+RATINGS_FILE_PATH = '/path/to/ratings.csv'
+WATCHLIST_FILE_PATH = '/path/to/watchlist.csv'
 
 # Log
 DO_LOG = True
